1792c77f5d6f154b1483aed6b684d7138ffde7d7
skorikartem <94537430+skorikartem@users.noreply.github.com>
2022-02-16 19:38:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//Завдання 1
+let h1 = document.getElementsByTagName('h1')
+let ul = document.getElementsByTagName('h1')
+let li = document.getElementsByTagName('li')
+
+//Завдання 2
+let children = document.body.childNodes
+let firstChild = document.body.firstChild
+let lastChild = document.body.lastChild
